d56c88c5ae10547d757acaf13d010c96ee14ecf4
rainAgain <2582253358@qq.com>
2019-03-18 17:26:10 +0800

    commit

--- js/index.js
+++ js/index.js
@@ -1,5 +1,12 @@
 (function(win) {
-	var $logo = $('#logo-main');
+    var $logo = $('#logo-main'),
+        $canvas = document.getElementById('canvas'),
+        ctx = $canvas.getContext("2d"),
+        canvasConfig = {
+            bg: "#00f",
+            width: 650,
+            height: 110
+        }
 
 	$logo.on('click', '.add-sub-title', function() {
 		var $this = $(this);
@@ -11,15 +18,61 @@
 		opacity: true
 	});
 
-	function renderLogo() {
-		var $canvas = document.getElementById('canvas');
-		var ctx = $canvas.getContext("2d");
+	function renderLogo(opt) {
+        var _bg = canvasConfig.bg,
+            _width = canvasConfig.width,
+            _height = canvasConfig.height;
 
+        $canvas.style.width = _width + 'px';
+        $canvas.style.height = _height + 'px';
+        
 		// ctx.lineWidth = 10;
 
-		ctx.fillStyle="#0000ff";
-		ctx.fillRect(0,0,650,110);
+		ctx.fillStyle = _bg;
+		ctx.fillRect(0,0,_width,_height);
 	}
 
 	renderLogo();
+
+    $('body').on('input propertychange', '#color-picker', function() {
+        canvasConfig.bg = $(this).val();
+        renderLogo();
+
+    }).on('input propertychange', "#logo-width", function() {
+        canvasConfig.width = $(this).val();
+        renderLogo();
+        
+    }).on('input propertychange', '#logo-height', function() {
+        canvasConfig.height = $(this).val();
+        renderLogo();
+        
+    });
+})(window);
+
+(function(win) {
+    var oFReader = new FileReader(), 
+        rFilter = /^(?:image\/bmp|image\/cis\-cod|image\/gif|image\/ief|image\/jpeg|image\/jpeg|image\/jpeg|image\/pipeg|image\/png|image\/svg\+xml|image\/tiff|image\/x\-cmu\-raster|image\/x\-cmx|image\/x\-icon|image\/x\-portable\-anymap|image\/x\-portable\-bitmap|image\/x\-portable\-graymap|image\/x\-portable\-pixmap|image\/x\-rgb|image\/x\-xbitmap|image\/x\-xpixmap|image\/x\-xwindowdump)$/i;
+    var $upload = document.getElementById("upload"),
+        $preview = document.getElementById("preview");
+
+    oFReader.onload = function (oFREvent) {
+        $preview.src = oFREvent.target.result;
+        // $preview.style.backgroundImage = 'url('+oFREvent.target.result+')';
+    };
+
+    // 导入图片
+    win.loadImageFile = function() {
+
+        if ($upload.files.length === 0) { 
+            return; 
+        }
+        var oFile = $upload.files[0];
+    
+        if (!rFilter.test(oFile.type)) { 
+            alert("You must select a valid image file!"); 
+            return; 
+        }
+        oFReader.readAsDataURL(oFile);
+    }
+    
 })(window);
